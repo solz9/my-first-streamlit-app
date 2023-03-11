5e9829fa237739f41b0d4fc311b58e8a769ab24f
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-11 10:38:50 +0700

    Update import turtle.py

--- import turtle.py
+++ import turtle.py
@@ -1,17 +1,13 @@
-
-
+# my-first-streamlit-app
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 a = st.number_input('tham số a')
 b = st.number_input('tham số b')
 if st.button('Gỉai'):
   if a == 0:
     if b == 0:
-      print('vô số nghiệm')
+      st.write('vô số nghiệm')
     if b != 0:
-      print('vô nghiệm')
+      st.write('vô nghiệm')
   if a != 0:
-    print('Phương trình có nghiệm là', -b/a)
-streamlit run app.py & npx localtunnel --port 8501
-
-
-
+    st.write('Phương trình có nghiệm là', -b/a)
+!streamlit run app.py & npx localtunnel --port 8501
