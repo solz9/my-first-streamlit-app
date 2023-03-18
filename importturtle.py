7ce0e13be57592ece462e7651354eb3ab6db92e5
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-18 19:49:29 +0700

    Update importturtle.py

--- importturtle.py
+++ importturtle.py
@@ -1,6 +1,7 @@
 # my-first-streamlit-app
 import streamlit as st
-st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
+st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')'
+st.write('Phương trình có dạng: ax + b = 0')
 a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 if st.button('Giải'):
