58778d74375a3e136954ab3929de6a8cac8d0ec5
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-18 18:18:45 +0700

    Update importturtle.py

--- importturtle.py
+++ importturtle.py
@@ -1,14 +1,14 @@
 # my-first-streamlit-app
 import streamlit as st
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
-a = st.number_input('tham số a')
-b = st.number_input('tham số b')
-if st.button('Gỉai'):
+a = st.number_input('Tham số a')
+b = st.number_input('Tham số b')
+if st.button('Giải'):
   if a == 0:
     if b == 0:
-      st.write('vô số nghiệm')
+      st.write('Phương trình có vô số nghiệm')
     if b != 0:
-      st.write('vô nghiệm')
+      st.write('Phương trình vô nghiệm')
   if a != 0:
     st.write('Phương trình có nghiệm là', -b/a)
 
